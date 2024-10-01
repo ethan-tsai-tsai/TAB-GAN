747b27c6420fb1f716f5d3d1430ca5c72c3109ee
turnips900518 <turnips900518@gmail.com>
2024-10-01 15:00:36 +0800

    調整 bayes optimization

--- .command.sh
+++ .command.sh
@@ -43,6 +43,6 @@ python eval.py --stock 2610 --cuda 3 &
 
 # bayes optimization
 nohup python -u bayes_optim.py --stock 0050 --cuda 6 > ./out/optim_0050.out 2>&1 &
-nohup python -u bayes_optim.py --stock 1101 --cuda 5 > ./out/optim_1101.out 2>&1 &
+nohup python -u bayes_optim.py --stock 1101 --cuda 3 > ./out/optim_1101.out 2>&1 &
 nohup python -u bayes_optim.py --stock 2330 --cuda 7 > ./out/optim_2330.out 2>&1 &
 nohup python -u bayes_optim.py --stock 2610 --cuda 4 > ./out/optim_2610.out 2>&1 &
